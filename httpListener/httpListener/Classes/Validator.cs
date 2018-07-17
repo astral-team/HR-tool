a505bac459662126e109bdffba086536ec12a3c3
Ptize <golubevivan-99@mail.ru>
2018-07-17 13:35:02 +0300

    Добавлен валидатор
    Добавлен новый класс.И всё. Больше ничегою

---
+++ httpListener/httpListener/Classes/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace httpListener.Classes
+{
+    class Validator
+    {
+        public static void CheckingTimeOfSession(Session session)
+        {
+            if (session.ExpTime <= DateTime.Now)
+            {
+                session.SessionKey = Guid.Empty;
+            }
+            else
+            {
+                if ((session.ExpTime - DateTime.Now) <= TimeSpan.FromMinutes(5))
+                {
+                    Console.WriteLine("Продлить?Y/N");
+                    string extension = Console.ReadLine();
+                    if (extension == "Y")
+                    {
+                        session.ExpTime = session.ExpTime.AddHours(2);
+                    }
+                }
+            }
+        }
+    }
+}
