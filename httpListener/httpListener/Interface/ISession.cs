8366caa85fb237ffb0c62b4d10f40d1b740220d8
Александр Полторацкий <pag.2010@ya.ru>
2018-07-17 10:25:36 +0300

    Резервная копия

---
+++ httpListener/httpListener/Interface/ISession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpListener
+{
+    interface ISession
+    {
+        System.Guid Id { get; set; }
+        System.Guid LoginId { get; set; }
+        System.Guid SessionKey { get; set; }
+        string ExpTime { get; set; }
+
+        Logins Logins { get; set; }
+    }
+}
